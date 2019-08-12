2b9722dd5fa5c45f94acec2b1d56e84b2f77046f
bbsmith24 <bbsmith024@gmail.com>
2019-08-12 08:39:14 -0400

    Update AnalogI2CSensor.ino
    2 channels - not final version, this is a kludge to get things running now

--- AnalogI2CSensor/AnalogI2CSensor.ino
+++ AnalogI2CSensor/AnalogI2CSensor.ino
@@ -1,25 +1,28 @@
 //
-//
+// 2 channel A2D I2C sensor
 //
 #include <Wire.h>
 
-#define ANALOGPIN A0
-#define LEDPIN 5
+#define STEERINGPIN A0
+#define THROTTLEPIN A1
+#define STEERING_LEDPIN 5
+#define THROTTLE_LEDPIN 6
 
-int brightness = 0;    // how bright the LED is
-float sensorRange[2];
+int steeringBrightness = 0;    // how bright the LED is
+int throttleBrightness = 0;    // how bright the LED is
+float steeringRange[2];
+float throttleRange[2];
 float sensorValue = 0;
-//float sensorPercent = 0;
-String pctStr;
+
 bool strReady = true;
 
 union DataPacket
 {
-  float dataValue;
-  char dataCharArray[4];
+  float dataValue[2];
+  char dataCharArray[8];
 };
 
-DataPacket sensorPercent;
+DataPacket sensorPacket;
 
 void setup() 
 {
@@ -28,36 +31,38 @@ void setup()
   Wire.begin(8);                // join i2c bus with address #8
   Wire.onRequest(requestEvent); // register event
   Serial.begin(9600);  // start serial for output
-  pinMode(LEDPIN, OUTPUT);
-  sensorRange[0] = 1024;
-  sensorRange[1] = -1024;
+
+  pinMode(STEERING_LEDPIN, OUTPUT);
+  steeringRange[0] = 1024;
+  steeringRange[1] = -1024;
+  
+  pinMode(THROTTLE_LEDPIN, OUTPUT);
+  throttleRange[0] = 1024;
+  throttleRange[1] = -1024;
 }
 
 void loop() 
 {
-  sensorValue = (float)(analogRead(ANALOGPIN));
-  sensorRange[0] = sensorRange[0] < sensorValue ? sensorRange[0] : sensorValue;
-  sensorRange[1] = sensorRange[1] > sensorValue ? sensorRange[1] : sensorValue;
-  sensorPercent.dataValue = ((sensorValue - sensorRange[0]) / (sensorRange[1]-sensorRange[0])) * 100.0;
+  sensorValue = (float)(analogRead(STEERINGPIN));
+  steeringRange[0] = steeringRange[0] < sensorValue ? steeringRange[0] : sensorValue;
+  steeringRange[1] = steeringRange[1] > sensorValue ? steeringRange[1] : sensorValue;
+  sensorPacket.dataValue[0] = ((sensorValue - steeringRange[0]) / (steeringRange[1]-steeringRange[0])) * 100.0;
+  
+  sensorValue = (float)(analogRead(THROTTLEPIN));
+  throttleRange[0] = throttleRange[0] < sensorValue ? throttleRange[0] : sensorValue;
+  throttleRange[1] = throttleRange[1] > sensorValue ? throttleRange[1] : sensorValue;
+  sensorPacket.dataValue[0] = ((sensorValue - throttleRange[0]) / (throttleRange[1]-throttleRange[0])) * 100.0;
 
-  strReady = false;
-  pctStr = String(sensorPercent.dataValue,2);
+  steeringBrightness = (int)(2.55 * sensorPacket.dataValue[0]);
+  analogWrite(STEERING_LEDPIN, steeringBrightness);
 
-  strReady = true;
-  Serial.println(pctStr);         // print the string
-  brightness = (int)(2.55 * sensorPercent.dataValue);
-  analogWrite(LEDPIN, brightness);
+  throttleBrightness = (int)(2.55 * sensorPacket.dataValue[1]);
+  analogWrite(THROTTLE_LEDPIN, steeringBrightness);
 }
 
 // function that executes whenever data is requested by master
 // this function is registered as an event, see setup()
 void requestEvent() 
 {
-  // one option
-  //Wire.beginTransmission();
-  //Wire.write(pctStr.c_str());  
-  //Wire.endTransmission();
-  // another option...
-  //Wire.write(pctStr.c_str());
-  Wire.write(sensorPercent.dataCharArray, 4);
+  Wire.write(sensorPacket.dataCharArray, 8);
 }   
