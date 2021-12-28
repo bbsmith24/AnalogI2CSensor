bc2c362d6f80a85397d76d7b11b7f64bfac82c8d
bbsmith24 <bbsmith024@gmail.com>
2021-12-28 09:54:32 -0500

    Update AnalogI2CSensor.ino

--- AnalogI2CSensor/AnalogI2CSensor.ino
+++ AnalogI2CSensor/AnalogI2CSensor.ino
@@ -1,8 +1,7 @@
 //
-// YamuraLog cockpit sensor 
-//   2 A2D channels (steering and throttle)
-//   1 Digital channel (brake on/off)
-//   I2C address configurable 0-31
+// ProMini combo sensor
+//   4 A2D channels
+//   8 Digital channels
 //  
 // for Arduino ProMini 5V
 // could also run on other Arduino boards
@@ -14,35 +13,25 @@
 #include <Wire.h>
 // uncomment for debug string output to serial
 //#define DEBUGSTR
-// sensor pins
-// A0 labeled A0
-// A1 labeled A1
-// BRAKE_PIN labeled D0
-// CLUTCH_PIN labeled D1
-#define STEERINGPIN A0
-#define THROTTLEPIN A1
-#define BRAKE_PIN  10
-#define CLUTCH_PIN  11
-// I2C address jumpers
-#define ADDR_0 5
-#define ADDR_1 6
-#define ADDR_2 7
-#define ADDR_3 8
-#define ADDR_4 9
 // I2C info
 #define STDMODE   100000 // 100K
 #define FASTMODE  400000 // 400K
 // data packet to send to master - 3 floats/longs (can mix), 12 characters
+#define DATA_SIZE 9
+struct SensorPacket
+{
+  byte digVal;
+  int a2dVal[4];
+};
 union DataPacket
 {
-  float dataValue[4];
-  long  intValue[4];
-  char dataCharArray[16];
-};
-// current A2D value
-float sensorValue = 0;
-// data
-DataPacket sensorPacket;
+  SensorPacket sensors;
+  char dataVal[DATA_SIZE];
+} dataPacket;
+
+byte digVal = 0;
+int a2dVal[4];
+char buffer[125];
 //
 // initialize - set pin modes, read address jumper pins, initialize I2C
 //              set up I2C data request callback
@@ -50,59 +39,49 @@ DataPacket sensorPacket;
 void setup() 
 {
   // digital pin setup
-  // for direct wire to brake voltage splitter
-  pinMode(BRAKE_PIN, INPUT);
-  pinMode(CLUTCH_PIN, INPUT_PULLUP);
-  pinMode(ADDR_0, INPUT_PULLUP);
-  pinMode(ADDR_1, INPUT_PULLUP);
-  pinMode(ADDR_2, INPUT_PULLUP);
-  pinMode(ADDR_3, INPUT_PULLUP);
-  pinMode(ADDR_4, INPUT_PULLUP);
+  // 8 digital channels
+  pinMode( 5, INPUT_PULLUP);
+  pinMode( 6, INPUT_PULLUP);
+  pinMode( 7, INPUT_PULLUP);
+  pinMode( 8, INPUT_PULLUP);
+  pinMode( 9, INPUT_PULLUP);
+  pinMode(10, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP);
   // set I2C clock
   Wire.setClock(FASTMODE);
   // read jumpers for address
-  int wireAddr = 0;
-  if(digitalRead(ADDR_0) == LOW)
-  { wireAddr +=  1; }
-  if(digitalRead(ADDR_1) == LOW)
-  { wireAddr +=  2; }
-  if(digitalRead(ADDR_2) == LOW)
-  { wireAddr +=  4; }
-  if(digitalRead(ADDR_3) == LOW)
-  { wireAddr +=  8; }
-  if(digitalRead(ADDR_4) == LOW)
-  { wireAddr += 16; }
-  Wire.begin(8);                // join i2c bus with address #8
+  int wireAddr = 0x08;
+  Wire.begin(wireAddr);         // join i2c bus with address #8
   Wire.onRequest(requestEvent); // register event
-
-  #ifdef DEBUGSTR 
-  Serial.begin(9600);
-  while (!Serial) { ; }
-  Serial.println("Cockpit throttle/steering/brake sensor");
-  String outStr = "I2C address " + String(wireAddr);
-  Serial.println(outStr);
-  #endif
 }
 //
 // read sensor values to sensorPacket
 //
 void loop() 
 {
-  long v = analogRead(STEERINGPIN);
-  sensorPacket.intValue[0] = v;
-  v = analogRead(THROTTLEPIN);
-  sensorPacket.intValue[1] = v;
-  v = (long)digitalRead(BRAKE_PIN);
-  sensorPacket.intValue[2] = v;
-  v = (long)digitalRead(CLUTCH_PIN);
-  sensorPacket.intValue[3] = v;
-  #ifdef DEBUGSTR 
-  String outStr = String(sensorPacket.intValue[0]) + " " +
-                  String(sensorPacket.intValue[1]) + " " +
-                  String(sensorPacket.intValue[2]) + " " +
-                  String(sensorPacket.intValue[3]);
-  Serial.println(outStr);
-  #endif
+  // read to temp values
+  digVal = 0;
+  digVal += digitalRead( 5);
+  digVal += (digitalRead( 6) << 1);
+  digVal += (digitalRead( 7) << 2);
+  digVal += (digitalRead( 8) << 3);
+  digVal += (digitalRead( 9) << 4);
+  digVal += (digitalRead(10) << 5);
+  digVal += (digitalRead(11) << 6);
+  digVal += (digitalRead(12) << 7);
+  a2dVal[0] = analogRead(0);
+  a2dVal[1] = analogRead(1);
+  a2dVal[2] = analogRead(2);
+  a2dVal[3] = analogRead(3);
+  // transfer temp values to structure after all reads are complete
+  // reduce risk of I2C request getting data partway through a read (not unlikely for digital reads)
+  // need to implement a 'data ready' request to get last complete read
+  dataPacket.sensors.digVal = digVal;
+  for(int idx = 0; idx < 4; idx++)
+  {
+    dataPacket.sensors.a2dVal[idx] = a2dVal[idx];
+  }
 }
 //
 // function that executes whenever data is requested by master
@@ -110,8 +89,5 @@ void loop()
 //
 void requestEvent() 
 {
-  #ifdef DEBUGSTR 
-  Serial.println("sent");
-  #endif
-  Wire.write(sensorPacket.dataCharArray, 16);
-}   
+  Wire.write(dataPacket.dataVal, DATA_SIZE);
+}
