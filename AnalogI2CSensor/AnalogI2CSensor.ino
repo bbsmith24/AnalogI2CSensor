999fe61136185dcef4e168d9af84831d5bb50ba4
bbsmith24 <bbsmith024@gmail.com>
2021-12-28 10:25:42 -0500

    Update AnalogI2CSensor.ino
    added debugging option, receive handler, stop scanning when any byte is received to prepare for sending

--- AnalogI2CSensor/AnalogI2CSensor.ino
+++ AnalogI2CSensor/AnalogI2CSensor.ino
@@ -12,7 +12,7 @@
 // 
 #include <Wire.h>
 // uncomment for debug string output to serial
-//#define DEBUGSTR
+//#define DEBUGPRINT
 // I2C info
 #define STDMODE   100000 // 100K
 #define FASTMODE  400000 // 400K
@@ -32,12 +32,17 @@ union DataPacket
 byte digVal = 0;
 int a2dVal[4];
 char buffer[125];
+bool scanPorts = true;
 //
 // initialize - set pin modes, read address jumper pins, initialize I2C
 //              set up I2C data request callback
 //
 void setup() 
 {
+  #ifdef DEBUGPRINT
+  Serial.begin(115200);
+  Serial.println("YamuraLog v4 (ProMini digital/A2D combo sensor)");
+  #endif
   // digital pin setup
   // 8 digital channels
   pinMode( 5, INPUT_PULLUP);
@@ -54,6 +59,7 @@ void setup()
   int wireAddr = 0x08;
   Wire.begin(wireAddr);         // join i2c bus with address #8
   Wire.onRequest(requestEvent); // register event
+  Wire.onReceive(receiveEvent); // register event
 }
 //
 // read sensor values to sensorPacket
@@ -62,25 +68,28 @@ void loop()
 {
   // read to temp values
   digVal = 0;
-  digVal += digitalRead( 5);
-  digVal += (digitalRead( 6) << 1);
-  digVal += (digitalRead( 7) << 2);
-  digVal += (digitalRead( 8) << 3);
-  digVal += (digitalRead( 9) << 4);
-  digVal += (digitalRead(10) << 5);
-  digVal += (digitalRead(11) << 6);
-  digVal += (digitalRead(12) << 7);
-  a2dVal[0] = analogRead(0);
-  a2dVal[1] = analogRead(1);
-  a2dVal[2] = analogRead(2);
-  a2dVal[3] = analogRead(3);
+  if(scanPorts) digVal += digitalRead( 5);
+  if(scanPorts) digVal += (digitalRead( 6) << 1);
+  if(scanPorts) digVal += (digitalRead( 7) << 2);
+  if(scanPorts) digVal += (digitalRead( 8) << 3);
+  if(scanPorts) digVal += (digitalRead( 9) << 4);
+  if(scanPorts) digVal += (digitalRead(10) << 5);
+  if(scanPorts) digVal += (digitalRead(11) << 6);
+  if(scanPorts) digVal += (digitalRead(12) << 7);
+  if(scanPorts) a2dVal[0] = analogRead(0);
+  if(scanPorts) a2dVal[1] = analogRead(1);
+  if(scanPorts) a2dVal[2] = analogRead(2);
+  if(scanPorts) a2dVal[3] = analogRead(3);
   // transfer temp values to structure after all reads are complete
   // reduce risk of I2C request getting data partway through a read (not unlikely for digital reads)
   // need to implement a 'data ready' request to get last complete read
-  dataPacket.sensors.digVal = digVal;
-  for(int idx = 0; idx < 4; idx++)
+  if(scanPorts) 
   {
-    dataPacket.sensors.a2dVal[idx] = a2dVal[idx];
+    dataPacket.sensors.digVal = digVal;
+    for(int idx = 0; idx < 4; idx++)
+    {
+      dataPacket.sensors.a2dVal[idx] = a2dVal[idx];
+    }
   }
 }
 //
@@ -90,4 +99,28 @@ void loop()
 void requestEvent() 
 {
   Wire.write(dataPacket.dataVal, DATA_SIZE);
+  #ifdef DEBUGPRINT
+  Serial.print("Sent ");
+  Serial.println(DATA_SIZE);
+  Serial.println(" bytes, scanPorts set to true");
+  #endif
+  scanPorts = true;
+}
+void receiveEvent()
+{
+  // at this point consider any send to this device a request to prepare to send data
+  #ifdef DEBUGPRINT
+  Serial.print("Received ");
+  #endif
+  while(Wire.available() > 1) 
+  {
+    char c = Wire.read();        // receive byte as a character
+    #ifdef DEBUGPRINT
+    Serial.print(c);
+    #endif
+  }
+  scanPorts = false;
+  #ifdef DEBUGPRINT
+  Serial.println(" scanPorts set to false");
+  #endif
 }
